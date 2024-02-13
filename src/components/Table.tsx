d47e617ffc0d325ddf2454aa8ec4b0631c347118
DaniLoraSe <danilo.lora@sequal.com.co>
2024-02-13 08:15:22 -0500

    Agregar PrimeReact y primereactIcons, tambien configurarlo pra proyecto y organizar estilos de tailwin para que no haga conflicto, hacer npm i antes de ejecutar proyecto

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,20 +1,59 @@
-import { DataTable } from "primereact/datatable";
-import { Column } from "primereact/column";
-import { defaultData } from "../utils/defaultData";
-import { useState } from "react";
-
-
+import React, { useState } from 'react';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { InputText } from 'primereact/inputtext';
+import { defaultData } from '../utils/defaultData';
+import { Dropdown } from 'primereact/dropdown';
 
 const Table = (): JSX.Element => {
-    const [data, setData] = useState(defaultData);
+  const [data, setData] = useState(defaultData);
+  const [globalFilter, setGlobalFilter] = useState('');
+
+
+
+  const statusFilter = (
+    <Dropdown
+      value={null}
+      onChange={(e) => {
+        setGlobalFilter('');
+        setData(defaultData.filter((item) => (e.value ? item.status === e.value : true)));
+      }}
+      placeholder="Select a Status"
+    />
+  );
+
+  const header = (
+    <div className="table-header">
+      <h5 className="p-m-0">Manage Licenses</h5>
+      <span className="p-input-icon-left">
+        <i className="pi pi-search" />
+        <InputText
+          type="search"
+          onInput={(e) => setGlobalFilter((e.target as HTMLInputElement).value)}
+          placeholder="Search..."
+        />
+      </span>
+    </div>
+  );
 
   return (
-    <DataTable value={data} removableSort paginator rows={5} rowsPerPageOptions={[5, 10, 25, 50]} tableStyle={{ minWidth: "40rem" }}>
-      <Column field="name" header="Name"></Column>
-      <Column field="lastName" header="Last Name"></Column>
-      <Column field="age" header="Age"></Column>
-      <Column field="status" header="Status"></Column>
-    </DataTable>
+    <div className="datatable">
+      <DataTable
+        value={data}
+        removableSort
+        paginator
+        rows={5}
+        rowsPerPageOptions={[5, 10, 25, 50]}
+        tableStyle={{ minWidth: '40rem', maxHeight: '400px', overflowY: 'auto' }}
+        globalFilter={globalFilter}
+        header={header}
+      >
+        <Column field="name" header="Name" sortable filter />
+        <Column field="lastName" header="Last Name" sortable filter />
+        <Column field="age" header="Age" sortable filter />
+        <Column field="status" header="Status" sortable filter />
+      </DataTable>
+    </div>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import 'primereact/resources/themes/saga-blue/theme.css'; // Escoge el tema que prefieras
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
