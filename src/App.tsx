7d9d914a51b2e2267129f4c4b93c50e396e37467
Sergio-Sequal <sergio.munoz@sequal.com.co>
2024-02-16 11:34:05 -0500

    Agregado el loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-
 import Layout from "./layout/Layout";
 import Loader from "./components/Loader";
-
-
+import { useEffect, useState } from "react";
 
 const App = (): JSX.Element => {
-  
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simula un tiempo de carga, por ejemplo, 2 segundos
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <div>
-      <Loader isActive={false}/>
-      <Layout/>      
+      {loading && <Loader />}
+      <Layout />
     </div>
-
   );
 };
 
--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,14 +1,13 @@
-interface Props {
-  isActive: boolean;
-}
+// interface Props {
+//   isActive: boolean;
+// }
 
-const Loader: React.FC<Props> = ({ isActive }) => {
+const Loader: React.FC = () => {
   return (
     <>
-      {isActive ? (
-        <div className="flex items-center justify-center absolute left-0 top-0 right-0 bottom-0 bg-black opacity-70 z-40 overflow-hidden">
-          <span
-            className="w-12 h-12 
+      <div className="flex items-center justify-center absolute left-0 top-0 right-0 bottom-0 bg-black opacity-80 z-40 overflow-hidden">
+        <span
+          className="w-12 h-12 
                         rounded-full 
                         inline-block 
 
@@ -36,42 +35,8 @@ const Loader: React.FC<Props> = ({ isActive }) => {
                         after:border-l-solid
                         after:border-l-transparent
                         animate-spin"
-          ></span>
-        </div>
-      ) : (
-        <div className="invisible flex items-center justify-center absolute left-0 top-0 right-0 bottom-0 bg-black opacity-70 z-40 overflow-hidden">
-          <span
-            className="w-12 h-12 
-                        rounded-full 
-                        inline-block 
-
-                        border-t-4
-                        border-t-solid
-                        border-t-blue-400
-
-                        border-r-4
-                        border-r-solid
-                        border-r-transparent
-                        box-border
-
-                        after:content-['']
-                        after:box-border
-                        after:absolute
-                        after:left-0
-                        after:top-0
-                        after:w-12
-                        after:h-12
-                        after:rounded-full
-                        after:border-b-4
-                        after:border-b-solid
-                        after:border-b-red-500
-                        after:border-l-4
-                        after:border-l-solid
-                        after:border-l-transparent
-                        animate-spin"
-          ></span>
-        </div>
-      )}
+        ></span>
+      </div>
     </>
   );
 };
